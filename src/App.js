c0c12380dedd882ba99640df075444d50223538f
Ahmed Atef <93956891+aa-ghazzy@users.noreply.github.com>
2025-03-26 16:25:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eveniet consectetur asperiores perferendis provident molestiae vel maxime, assumenda excepturi dolor ad illum. Ad labore suscipit quam soluta.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eveniet consectetur asperiores perferendis provident molestiae vel maxime, assumenda excepturi dolor ad illum. Ad labore suscipit quam soluta.",
+  },
+  {
+    title: "Do you ship to countries oustide the EU?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eveniet consectetur asperiores perferendis provident molestiae vel maxime, assumenda excepturi dolor ad illum. Ad labore suscipit quam soluta.",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, index) => (
+        <AccordionItem
+          index={index + 1}
+          title={item.title}
+          text={item.text}
+          key={index}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ index, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleIsOpen() {
+    setIsOpen(() => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleIsOpen}>
+      <p className="number">{index < 10 ? `0${index}` : index}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen ? <div className="content-box">{text}</div> : ""}
+    </div>
+  );
+}
